ea8b178bd21a2b010d0cc70a832c1e8580d9d935
Chris Morris <chrismorris08@gmail.com>
2020-09-26 14:28:54 -0500

    heroku setup

---
+++ app_server/models/cards.js
@@ -0,0 +1,36 @@
+const mongoose = require( 'mongoose' );
+const cardSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    number: {
+        type: Number,
+        required: true
+    },
+    arcana: {
+        type: String,
+        required: true
+    },
+    suit: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    uprightKeywords: {
+        type: String,
+        required: true
+    },
+    reversedKeywords: {
+        type: String,
+        required: true
+    },
+    archetype: {
+        type: String,
+        required: false
+    },
+});
+mongoose.model('cards', cardSchema);
