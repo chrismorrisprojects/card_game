1cf9f0f974bb2e41c01dc6f4d215bbcce8402a9c
Chris Morris <chrismorris08@gmail.com>
2020-09-28 17:31:28 -0500

    Added 10 card random draw for Celtic cross.

--- app_api/controllers/cards.js
+++ app_api/controllers/cards.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const mdlCard = mongoose.model('cards');
+const celticCrossAmt = 10;
+
 const cardsReadOne = (req, res) => {
     mdlCard
         .findOne({name: req.params.cardname})
@@ -21,27 +23,42 @@ const cardsReadOne = (req, res) => {
 };
 
 
+
 const cardRandom = (req, res) => {
-    console.log("test");
-    let randomCard = Math.floor(Math.random() * 78)
-    console.log("random number: " + randomCard);
-    mdlCard
-        .findOne().skip(randomCard)
-        .exec((err, card) => {
-            if (!card) {
-                return res
-                    .status(404)
-                    .json({"message": "card not found"});
-            } else if (err) {
-                return res
-                    .status(404)
-                    .json(err);
-            } else {
-                return res
-                    .status(200)
-                    .json(card);
-            }
-        });
+    let i;
+    let cardsToDraw = [];
+    let drawnCards = [];
+    for (i = 0; i < celticCrossAmt; i++) {
+        let randomCard = Math.floor(Math.random() * 78);
+        cardsToDraw.push(randomCard);
+    }
+    console.log(cardsToDraw);
+    for (let i = 0; i < cardsToDraw.length; i++){
+        mdlCard
+            .findOne().skip(cardsToDraw[i])
+            .exec((err, card) =>{
+                if (!card) {
+                    return res
+                        .status(404)
+                        .json({"message": "card not found"});
+                } else if (err) {
+                    return res
+                        .status(404)
+                        .json(err);
+                } else {
+                    console.log(typeof card);
+                    drawnCards.push(card);
+                    if (i === celticCrossAmt - 1) {
+                        return res
+                            .status(200)
+                            .json(drawnCards);
+                    }
+                }
+
+
+            });
+    }
+
 };
 
 const cardCreate = (req, res) => {
