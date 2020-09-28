11e53679ff7bffe820db08e9b849635241522cbd
Chris Morris <chrismorris08@gmail.com>
2020-09-28 18:12:37 -0500

    Fixed random draw being broken.

--- app_server/controllers/cards.js
+++ app_server/controllers/cards.js
@@ -1,22 +1,36 @@
-const request = require('request');
-const apiOptions = {
-    server: 'http://localhost:3000'
+//const request = require('request');
+//const apiOptions = {
+//    server: 'http://localhost:3000'
+//};
+//
+//const celticCross = (req, res) =>{
+//
+//}
+//
+//const getCelticCross = (req, res, callback) => {
+//    const apiPath = '/api/cards/draw';
+//    const requestOptions = {
+//        url: '${apiOptions.server}${path}',
+//        method: 'GET',
+//        json: {}
+//    };
+//
+//}
+//
+//const renderCelticCross = (req, res) => {
+//
+//}
+const cardList = (req, res) => {
+    res.render('card-list', {
+        title: 'List of Tarot Cards',
+        pageHeader:{
+            title: 'Tarot Cards',
+            strapLine: 'Digital tarot card readings you control!'
+        },
+        sidebar: "Digital tarot card reading to help you navigate life."
+    });
 };
 
-const celticCross = (req, res) =>{
-
-}
-
-const getCelticCross = (req, res, callback) => {
-    const apiPath = '/api/cards/draw';
-    const requestOptions = {
-        url: '${apiOptions.server}${path}',
-        method: 'GET',
-        json: {}
-    };
-
-}
-
-const renderCelticCross = (req, res) => {
-
-}
+module.exports = {
+    cardList
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -7,8 +7,12 @@ const ctrlCards = require('../controllers/cards');
 
 /* GET home page. */
 
-router
-    .route('/cards/celticCross')
-    .get(ctrlCards.celticCross);
+//router
+//    .route('/cards/celticCross')
+//    .get(ctrlCards.celticCross);
+
+router.get('/cards', ctrlCards.cardList);
 
 module.exports = router;
+
+//module.exports = router;
