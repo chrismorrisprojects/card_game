f0cea4512993bde9c663bbdd47f49e797486d49c
Chris Morris <chrismorris08@gmail.com>
2020-10-18 13:56:26 -0500

    Fixed port issue and aded port change based off of environment.

--- app.js
+++ app.js
@@ -12,9 +12,19 @@ const apiRouter = require('./app_api/routes/index');
 
 const app = express();
 //test2
-var listener = app.listen(3001, function(){
-  console.log('Listening on port ' + listener.address().port); //Listening on port 8888
-});
+
+if (process.env.ENVIRONMENT === 'production') {
+  var listener = app.listen(3000, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+  });
+}
+
+if (process.env.ENVIRONMENT === 'dev') {
+  var listener = app.listen(3001, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+  });
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
